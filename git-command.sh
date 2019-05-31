3f75eb320ea929ecd0b71156d47e37f9f7eeb47b
CreatorWithLove <CreatorWithLove@outlook.com>
2019-05-31 14:03:12 +0800

    dev 1

--- git-command.sh
+++ git-command.sh
@@ -48,3 +48,18 @@ git checkout -- src/Hello.java
 # 删除一个已经提交的文件
 # 已经commit的无法彻底删除,可以通过版本回退找到
 # 提交一个已经删除的文件,可以在最新的版本中消除.但历史版本还有
+
+# 查看分支,绿色的带星的是当前的分支
+git branch -v
+
+# 新建的一个分支
+git branch dev
+
+# 切换分支
+git checkout dev
+
+# 合并分支
+# 从把dev的东西合并到master
+# 在master上,从dev往master拉
+git branch master
+git merge dev
